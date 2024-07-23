2ff8f625605450cc5e87886816f90e72dd8bc569
Submerg <submergthefurry@gmail.com>
2024-07-23 14:16:00 -0400

    Initial Setup

--- src/main/java/net/submerg/tembris/Tembris.java
+++ src/main/java/net/submerg/tembris/Tembris.java
@@ -19,7 +19,7 @@ public class Tembris {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tembris";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public Tembris() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
